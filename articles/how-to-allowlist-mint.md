7020ba536bd9b6dd1c698e1e324ede4a96cee68a
Junya Kono <junya03dance@gmail.com>
2023-05-17 16:17:44 +0900

    feat: contract

---
+++ articles/how-to-allowlist-mint.md
@@ -0,0 +1,7 @@
+---
+title: "AllowList を用いた NFT の mint"
+emoji: "🌲"
+type: "tech" # tech: 技術記事 / idea: アイデア
+topics: ["nft", "merkletree"]
+published: false
+---
---
+++ examples/how-to-allowlist-mint/contract/src/ERC721.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MyERC721 is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    bytes32 public merkleRoot;
+
+    constructor() ERC721("MyERC721", "MY721") {}
+
+    function mint(
+        bytes32[] calldata _merkleProof
+    ) public payable returns (uint256) {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+            "Invalid proof"
+        );
+
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
+        return newTokenId;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+}
